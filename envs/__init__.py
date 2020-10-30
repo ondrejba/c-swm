f90c6cbc3c7390d7327d2d899c75c1b5559b43bd
Ondrej Biza <ondrej.biza@gmail.com>
2020-10-30 18:38:15 -0400

    test the cursor environment

--- envs/__init__.py
+++ envs/__init__.py
@@ -15,6 +15,20 @@ register(
 )
 
 register(
+    'ShapesCursorTrain-v0',
+    entry_point='envs.block_pushing_cursor:BlockPushingCursor',
+    max_episode_steps=100,
+    kwargs={'render_type': 'shapes'},
+)
+
+register(
+    'ShapesCursorEval-v0',
+    entry_point='envs.block_pushing_cursor:BlockPushingCursor',
+    max_episode_steps=10,
+    kwargs={'render_type': 'shapes'},
+)
+
+register(
     'CubesTrain-v0',
     entry_point='envs.block_pushing:BlockPushing',
     max_episode_steps=100,
